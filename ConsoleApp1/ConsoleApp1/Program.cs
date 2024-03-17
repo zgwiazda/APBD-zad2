4fecaa708e1305b2e78ca53daa326dc86407aba0
zgwiazda <s26615@pjwstk.edu.pl>
2024-03-17 13:52:57 +0100

    "GetMax method"

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,3 +16,21 @@ static double GetAverage(int[] numbers)
 int[] arr = {1,2,3,4};
 double avg = GetAverage(arr);
 Console.WriteLine(avg);
+
+static int GetMax(int[] num)
+{
+    int current_max = num[0];
+    for (int i = 1; i < num.Length; i++)
+    {
+        if (current_max < num[i])
+            current_max = num[i];
+    }
+
+    return current_max;
+}
+
+
+
+int[] num = { 1, 2, 3, 4, 5, 6 };
+int max = GetMax(num);
+Console.WriteLine(max);
